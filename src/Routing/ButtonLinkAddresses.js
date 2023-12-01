ba6f3885e5c2f1c0f6bda4d50293ab5115925a39
ClassOutside <messageclassoutside@gmail.com>
2023-11-30 21:55:14 -0500

    Remove Link
    Remove link to be added during video

--- src/Routing/ButtonLinkAddresses.js
+++ src/Routing/ButtonLinkAddresses.js
@@ -3,7 +3,6 @@ class ButtonLinkAddresses {
     this.default_button_address = "https://www.youtube.com/@classoutside";
     this.button_002_address =
       "https://github.com/ClassOutside?tab=repositories";
-    this.button_004_address = "https://youtu.be/81G_ACQrA34";
   }
 }
 
